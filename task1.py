11ce381375f45cea3206ef4758c16aed547adf4d
dexstrong <ilya.yurev.04@inbox.ru>
2023-12-17 22:09:53 +0300

    Добавлена программа для определения знака числа

---
+++ task1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def test():
+    num = int(input("Введите целое число: "))
+    if num > 0:
+        positive()
+    elif num < 0:
+        negative()
+
+
+def positive():
+    print("Положительное")
+
+
+def negative():
+    print("Отрицательное")
+
+
+if __name__ == '__main__':
+    test()
